594a2c41a1746afa751d9b6e16e4b776b9421a1c
Scoops Martin <michael.martin@cleancoders.com>
2025-09-19 19:53:16 -0500

    formatting

--- src/mcp/server/errors.clj
+++ src/mcp/server/errors.clj
@@ -1,15 +1,15 @@
 (ns mcp.server.errors)
 
 (def errors
-  {:invalid-request {:code -32600 :message "Invalid Request"}
-   :uninitialized {:code -32002 :message "Server not initialized"}
+  {:invalid-request      {:code -32600 :message "Invalid Request"}
+   :uninitialized        {:code -32002 :message "Server not initialized"}
    :unsupported-protocol {:code -32602 :message "Unsupported protocol version"}})
 
 (defn ->rpc-error
   ([kind data] (->rpc-error kind nil data))
   ([kind id data] {:jsonrpc "2.0"
-                   :id id
-                   :error (merge (kind errors) {:data data})}))
+                   :id      id
+                   :error   (merge (kind errors) {:data data})}))
 
 (defn uninitialized [id]
   (->rpc-error :uninitialized id "Must initialize connection before invoking methods"))
